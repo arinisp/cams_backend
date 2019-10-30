d783e502bf9ccc71084a67d7865b2ca823d27683
Arini Sri Purbani <arinisp@gmail.com>
2019-10-30 10:44:46 +0700

    Django_02_migration_customer_model

--- customerapp/models.py
+++ customerapp/models.py
@@ -1,10 +1,26 @@
 from django.db import models
 
+JENIS_KELAMIN = (
+    ('L', 'Laki-Laki'),
+    ('P', 'Perempuan')
+)
+
+OCCUPATION = (
+    ('n', 'Non-Fix'),
+    ('f', 'Fix')
+)
+
 # Create your models here.
 class Customer (models.Model):
     name = models.fields.CharField(max_length=255)
+    jenis_kelamin = models.fields.CharField(max_length=1, choices=JENIS_KELAMIN, default='L')
+    total_income = models.fields.FloatField()
+    existing = models.fields.BooleanField()
+    occupation = models.fields.CharField(max_length=1, choices=OCCUPATION, default='n')
+    alamat = models.fields.TextField()
 
     def __str__(self):
         return self.name
     def __repr__(self):
         return self.name
+

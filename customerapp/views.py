7735c6b6e930857ae0731d9cd36371ffde860370
Arini Sri Purbani <arinisp@gmail.com>
2019-10-30 13:26:59 +0700

    Django_04_import_csv

--- customerapp/views.py
+++ customerapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+import csv
+from django.http import HttpResponse
+from customerapp.models import Customer
+
+def customer_csv (request):
+    response = HttpResponse(content_type = 'text/csv')
+    response['Content-Disposition'] = 'attachment; filename=customerapp.csv'
+
+    writer = csv.writer(response)
+    customers = Customer.objects.all()
+
+    writer.writerow(['Name', 'Jenis Kelamin', 'Total Income', 'Existing', 'Occupation', 'Alamat', 'Kendaraan'])
+    for customer in customers:
+        writer.writerow([customer.name, customer.jenis_kelamin, customer.total_income, customer.existing
+        , customer.occupation, customer.alamat, customer.kendaraan])
+
+    return response
